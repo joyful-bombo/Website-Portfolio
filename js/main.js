75ee57957af74d9643a191fa9097feb8e03ab1a0
Moon Bombo <bombo96@naver.com>
2022-03-09 00:57:19 +0900

    js navbar__menu, contact  btn click scroll to section

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 'use strict';
 
+// navbar
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
+// navbar__menu btn
+const navbarMenu = document.querySelector('.navbar__menu');
+
+// home__contact btn
+const contactBtn = document.querySelector('.home__contact');
+
+// navbar is transparent on scroll top else blue
 document.addEventListener('scroll' , (e) => {
-  console.log(`window.scrollY : ${window.scrollY}`);
-  console.log(`navbarHeight : ${navbarHeight}`);
   if (window.scrollY > navbarHeight){
     navbar.classList.add('sticky');
   }
@@ -13,3 +19,23 @@ document.addEventListener('scroll' , (e) => {
     navbar.classList.remove('sticky');
   }
 });
+
+// navbar click menu to scroll Update
+navbarMenu.addEventListener('click', (e) => {
+  console.log(e.target.dataset.link);
+  const target = e.target;
+  const link = target.dataset.link;
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+contactBtn.addEventListener('click', () => {
+  const contact = document.querySelector('#contact');
+  contact.scrollIntoView({
+    behavior : "smooth"
+  });
+});
+
